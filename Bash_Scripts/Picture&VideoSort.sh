ed23a23d00f73a137e24fff8370a221e9c161512
Wesley Fister <112534696+WesleyFister@users.noreply.github.com>
2023-02-15 10:30:48 -0800

    Update Picture&VideoSort.sh

--- Bash_Scripts/Picture&VideoSort.sh
+++ Bash_Scripts/Picture&VideoSort.sh
@@ -3,16 +3,26 @@
 #
 # find . -type f \( -iname "*.jpg" \) -exec exiftool -d ~/Pictures/"TimeStampedPictures&Videos"/TimeStampedPictures/'%Y-%m-%d_%H-%M-%S.jpg' '-filename<CreateDate' {} \;
 # This command will rename the file by its creation date and move it to another directory.
+#
+# TODO: Add deduplication to script's scope. 
+
+picture_sort(){
+	find . -type f \( -iname "*$1" \) $2 \; -exec exiftool -d /media/flip/"8TB HDD/Other/Pictures&Videos/Family Pictures/%Y/%Y-%m-%d_%H-%M-%S%%-c$3" '-filename<CreateDate' {} \; -exec mv {} /media/flip/"8TB HDD/Other/Pictures&Videos/Family Pictures/Unkown Date" \;
+}
+
+video_sort(){
+	find . -type f \( -iname "*$1" \) -exec exiftool -d /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/%Y/%Y-%m-%d_%H-%M-%S%%-c$1" '-filename<CreateDate' {} \; -exec mv {} /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/Unkown Date" \;
+}
 
 while true; do
-find . -type f \( -iname "*.jpg" \) -exec jpegoptim -t {} \; -exec exiftool -d /media/flip/"8TB HDD/Other/Pictures&Videos/Family Pictures/%Y/%Y-%m-%d_%H-%M-%S%%-c.jpg" '-filename<CreateDate' {} \; -exec mv {} /media/flip/"8TB HDD/Other/Pictures&Videos/Family Pictures/Unkown Date" \; >> pictureVideoSort.log
-find . -type f \( -iname "*.jpeg" \) -exec jpegoptim -t {} \; -exec exiftool -d /media/flip/"8TB HDD/Other/Pictures&Videos/Family Pictures/%Y/%Y-%m-%d_%H-%M-%S%%-c.jpg" '-filename<CreateDate' {} \; -exec mv {} /media/flip/"8TB HDD/Other/Pictures&Videos/Family Pictures/Unkown Date" \; >> pictureVideoSort.log
-find . -type f \( -iname "*.png" \) -exec optipng -o7 {} \; -exec exiftool -d /media/flip/"8TB HDD/Other/Pictures&Videos/Family Pictures/%Y/%Y-%m-%d_%H-%M-%S%%-c.png" '-filename<CreateDate' {} \; -exec mv {} /media/flip/"8TB HDD/Other/Pictures&Videos/Family Pictures/Unkown Date" \; >> pictureVideoSort.log
+	picture_sort .jpg "-exec jpegoptim -t {}" .jpg >> pictureVideoSort.log
+	picture_sort .jpeg "-exec jpegoptim -t {}" .jpg >> pictureVideoSort.log
+	picture_sort .png "-exec optipng -o7 {}" .png >> pictureVideoSort.log
 
-find . -type f \( -iname "*.mp4" \) -exec exiftool -d /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/%Y/%Y-%m-%d_%H-%M-%S%%-c.mp4" '-filename<CreateDate' {} \; -exec mv {} /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/Unkown Date" \; >> pictureVideoSort.log
-find . -type f \( -iname "*.mkv" \) -exec exiftool -d /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/%Y/%Y-%m-%d_%H-%M-%S%%-c.mkv" '-filename<CreateDate' {} \; -exec mv {} /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/Unkown Date" \; >> pictureVideoSort.log
-find . -type f \( -iname "*.mov" \) -exec exiftool -d /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/%Y/%Y-%m-%d_%H-%M-%S%%-c.mov" '-filename<CreateDate' {} \; -exec mv {} /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/Unkown Date" \; >> pictureVideoSort.log
-find . -type f \( -iname "*.webm" \) -exec exiftool -d /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/%Y/%Y-%m-%d_%H-%M-%S%%-c.webm" '-filename<CreateDate' {} \; -exec mv {} /media/flip/"8TB HDD/Other/Pictures&Videos/Family Videos/Unkown Date" \; >> pictureVideoSort.log
+	video_sort .mp4 >> pictureVideoSort.log
+	video_sort .mkv >> pictureVideoSort.log
+	video_sort .mov >> pictureVideoSort.log
+	video_sort .webm >> pictureVideoSort.log
 
-sleep 1m
+	sleep 1m
 done
