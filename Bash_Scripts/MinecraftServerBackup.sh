e26c99179cd6d0134947b16d993bf37f115af962
Wesley Fister <112534696+WesleyFister@users.noreply.github.com>
2024-02-16 11:00:57 -0800

    Delete Bash_Scripts/MinecraftServerBackup.sh

--- Bash_Scripts/MinecraftServerBackup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-tmux send-keys -t 0 save-off ENTER
-tmux send-keys -t 0 "say Saving world in 5 seconds!" ENTER
-sleep 5
-tmux send-keys -t 0 save-all ENTER
-tmux send-keys -t 0 "say Saving complete!" ENTER
-7z a /media/flip/8TB\ HDD/Games/Videogame\ Saves/Computer/Minecraft\ Java/Wesley\&Matt\ \(`date +%Y-%m-%d`\) /home/flip/Documents/Minecraft\ Server/Wesley\&Matt
-tmux send-keys -t 0 save-on ENTER
